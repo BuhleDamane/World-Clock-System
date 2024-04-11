ce0eca7a37f1d08b760e0541a2fb992b4b926b6c
BuhleDamane <ziziphodamane@gmail.com>
2024-04-11 21:14:47 +0200

    updated all my files

---
+++ java.js
@@ -0,0 +1,10 @@
+let worldClockHomepageLink = document.querySelector(".worldClockHomepageLink");
+let worldClockAlarmLink = document.querySelector(".worldClockAlarm");
+let worldClockStopwatchLink = document.querySelector(".worldClockStopwatchLink");
+let worldClockTimerLink = document.querySelector(".worldClockTimerLink");
+let wrapper = document.querySelector(".wrapper");
+
+worldClockHomepageLink.addEventListener("click", () =>{
+wrapper.classList.toggle("active")
+});
+
